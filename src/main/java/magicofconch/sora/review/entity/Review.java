4fa5f839e47f5dd4b1118aa8512174b9c3960139
greensnapback0229 <smdmim@gmail.com>
2024-09-05 01:04:14 +0900

    feat: add entity mapping

--- src/main/java/magicofconch/sora/review/entity/Review.java
+++ src/main/java/magicofconch/sora/review/entity/Review.java
@@ -4,16 +4,25 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import magicofconch.sora.review.enums.ReviewType;
+import magicofconch.sora.user.entity.UserInfo;
 
 @Entity
 public class Review {
 	@Id
+	@Column(name = "review_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "userinfo_id", insertable = false, updatable = false)
+	private UserInfo userInfo;
 
 	@Enumerated(EnumType.STRING)
 	private ReviewType type;
--- src/main/java/magicofconch/sora/user/entity/UserInfo.java
+++ src/main/java/magicofconch/sora/user/entity/UserInfo.java
@@ -1,16 +1,28 @@
 package magicofconch.sora.user.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import magicofconch.sora.review.entity.Review;
 
 @Entity
 public class UserInfo {
     @Id
+    @Column(name = "userInfo_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private List<Review> reviews = new ArrayList<Review>();
     private String uuid;
     private String socialId;
     private String mobileOsId;
