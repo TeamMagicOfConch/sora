40d801e7c3167f446e0a2274f1730020e792c7b2
greensnapback0229 <smdmim@gmail.com>
2024-09-17 03:58:01 +0900

    feat: add BaseEntity for UserEntity

--- src/main/java/magicofconch/sora/user/entity/UserInfo.java
+++ src/main/java/magicofconch/sora/user/entity/UserInfo.java
@@ -12,16 +12,23 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import magicofconch.sora.review.entity.Review;
+import magicofconch.sora.util.BaseEntity;
 
+@Getter
 @Entity
-public class UserInfo {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserInfo extends BaseEntity {
     @Id
     @Column(name = "user_info_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "review_id")
     private List<Review> reviews = new ArrayList<Review>();
     private String uuid;
@@ -32,4 +39,17 @@ public class UserInfo {
     private List<OsAuthInfo> osAuthInfo = new ArrayList<OsAuthInfo>();
 
     private Integer initialReviewCount;
+
+    private String role;
+
+
+    @Builder
+    public UserInfo(String uuid, String socialId, OsAuthInfo osAuthInfo, String role){
+        this.uuid = uuid;
+        this.socialId = socialId;
+        this.osAuthInfo.add(osAuthInfo);
+        this.role = role;
+    }
+
+
 }
