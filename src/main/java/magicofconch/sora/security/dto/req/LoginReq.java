aec55782b9bccd6c6b3c45d64f2b224e5fbea72f
greensnapback0229 <smdmim@gmail.com>
2024-09-25 03:28:34 +0900

    feat: add login dto

---
+++ src/main/java/magicofconch/sora/security/dto/req/LoginReq.java
@@ -0,0 +1,11 @@
+package magicofconch.sora.security.dto.req;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginReq {
+
+	private String osId;
+}
