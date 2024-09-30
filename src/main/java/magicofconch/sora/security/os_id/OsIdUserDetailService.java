3d46ea86ff0a7f2357efc85b17fba8a81c63da90
greensnapback0229 <smdmim@gmail.com>
2024-10-01 02:54:39 +0900

    feat: apply redirect for login to register

--- src/main/java/magicofconch/sora/security/os_id/OsIdUserDetailService.java
+++ src/main/java/magicofconch/sora/security/os_id/OsIdUserDetailService.java
@@ -10,6 +10,7 @@ import magicofconch.sora.security.CustomUserDetails;
 import magicofconch.sora.security.UserDto;
 import magicofconch.sora.user.entity.OsAuthInfo;
 import magicofconch.sora.user.repository.OsAuthInfoRepository;
+import magicofconch.sora.util.exception.RedirectException;
 
 @Slf4j
 @Service
@@ -24,7 +25,7 @@ public class OsIdUserDetailService implements UserDetailsService {
 	public CustomUserDetails loadUserByUsername(String osId) throws UsernameNotFoundException {
 
 		OsAuthInfo osAuthInfo = osAuthInfoRepository.findOsAuthInfoByOsId(osId)
-			.orElseThrow(() -> new UsernameNotFoundException("없는 os_id 입니다"));
+			.orElseThrow(() -> new RedirectException("/user/register"));
 
 		CustomUserDetails userDetails = new CustomUserDetails(UserDto.fromUserInfo(osAuthInfo.getUserInfo(), osAuthInfo.getOsId()));
 
