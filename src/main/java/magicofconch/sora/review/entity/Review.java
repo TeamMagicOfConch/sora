ec71d1210d7ead9e02344043ebf6712a95592f4d
greensnapback0229 <smdmim@gmail.com>
2024-10-12 19:27:35 +0900

    feat: add reviewDate for review

--- src/main/java/magicofconch/sora/review/entity/Review.java
+++ src/main/java/magicofconch/sora/review/entity/Review.java
@@ -1,50 +1,55 @@
-package magicofconch.sora.review.entity;
+	package magicofconch.sora.review.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import magicofconch.sora.review.enums.FeedbackType;
-import magicofconch.sora.user.entity.UserInfo;
-import magicofconch.sora.util.BaseEntity;
+	import java.time.LocalDate;
 
-@Entity
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Review extends BaseEntity {
-	@Id
-	@Column(name = "review_id")
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	import jakarta.persistence.Column;
+	import jakarta.persistence.Entity;
+	import jakarta.persistence.EnumType;
+	import jakarta.persistence.Enumerated;
+	import jakarta.persistence.FetchType;
+	import jakarta.persistence.GeneratedValue;
+	import jakarta.persistence.GenerationType;
+	import jakarta.persistence.Id;
+	import jakarta.persistence.JoinColumn;
+	import jakarta.persistence.ManyToOne;
+	import lombok.AccessLevel;
+	import lombok.Builder;
+	import lombok.Getter;
+	import lombok.NoArgsConstructor;
+	import magicofconch.sora.review.enums.FeedbackType;
+	import magicofconch.sora.user.entity.UserInfo;
+	import magicofconch.sora.util.BaseEntity;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "review_id", insertable = false, updatable = false)
-	private UserInfo userInfo;
+	@Entity
+	@Getter
+	@NoArgsConstructor(access = AccessLevel.PROTECTED)
+	public class Review extends BaseEntity {
+		@Id
+		@Column(name = "review_id")
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private Long id;
 
-	@Enumerated(EnumType.STRING)
-	private FeedbackType feedbackType;
+		@ManyToOne(fetch = FetchType.LAZY)
+		@JoinColumn(name = "review_id", insertable = false, updatable = false)
+		private UserInfo userInfo;
 
-	@Column(columnDefinition = "TEXT", nullable = false)
-	private String body;
+		@Enumerated(EnumType.STRING)
+		private FeedbackType feedbackType;
 
-	@Column(columnDefinition = "TEXT", nullable = false)
-	private String feedback;
+		@Column(columnDefinition = "TEXT", nullable = false)
+		private String body;
 
-	@Builder
-	public Review(UserInfo userInfo, FeedbackType feedbackType, String body, String feedback){
-		this.userInfo = userInfo;
-		this.feedbackType = feedbackType;
-		this.body = body;
-		this.feedback = feedback;
+		@Column(columnDefinition = "TEXT", nullable = false)
+		private String feedback;
+
+		private LocalDate reviewDate;
+
+		@Builder
+		public Review(UserInfo userInfo, FeedbackType feedbackType, String body, String feedback, LocalDate reviewDate){
+			this.userInfo = userInfo;
+			this.feedbackType = feedbackType;
+			this.body = body;
+			this.feedback = feedback;
+			this.reviewDate = reviewDate;
+		}
 	}
-}
