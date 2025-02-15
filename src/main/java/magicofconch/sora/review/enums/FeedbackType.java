f9e0f66e012f9e20debceb22272112e4e340b067
greensnapback0229 <smdmim@gmail.com>
2025-02-15 18:43:44 +0900

    fix($Review): add option for serialize

--- src/main/java/magicofconch/sora/review/enums/FeedbackType.java
+++ src/main/java/magicofconch/sora/review/enums/FeedbackType.java
@@ -1,15 +1,17 @@
 package magicofconch.sora.review.enums;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 
 @Getter
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum FeedbackType {
-	FEELING("Feeling"),
-	THINKING("Thinking");
+    FEELING("Feeling"),
+    THINKING("Thinking");
 
-	private final String value;
+    private final String value;
 
-	FeedbackType(String value){
-		this.value = value;
-	}
+    FeedbackType(String value) {
+        this.value = value;
+    }
 }
