e4bf13603679395fcfe81f9fd7c7be7d1d74aa5a
greensnapback0229 <smdmim@gmail.com>
2025-04-28 17:37:44 +0900

    chore($Sora): 엔티티 스캔 범위 추가

--- sora/src/main/java/magicofconch/sora/SoraApplication.java
+++ sora/src/main/java/magicofconch/sora/SoraApplication.java
@@ -2,9 +2,11 @@ package magicofconch.sora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"magicofconch", "magicOfConch"})
 @EnableJpaAuditing
 public class SoraApplication {
 
