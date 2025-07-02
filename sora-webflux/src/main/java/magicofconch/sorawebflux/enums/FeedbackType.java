1e152c417d25abed893cf3aa307eac575d23091c
greensnapback0229 <smdmim@gmail.com>
2025-07-02 17:43:25 +0900

    feat($Webflux): sora review 관련 dto 추가

---
+++ sora-webflux/src/main/java/magicofconch/sorawebflux/enums/FeedbackType.java
@@ -0,0 +1,17 @@
+package magicofconch.sorawebflux.enums;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+@Getter
+@JsonFormat(shape = JsonFormat.Shape.STRING)
+public enum FeedbackType {
+    FEELING("Feeling"),
+    THINKING("Thinking");
+
+    private final String value;
+
+    FeedbackType(String value) {
+        this.value = value;
+    }
+}
