96012ae5a63338b4dcfcdc4633c993195d07406d
greensnapback0229 <smdmim@gmail.com>
2025-09-10 00:25:03 +0900

    fix: 글로벌 에러 핸들러 status 미반영 문제 해결

--- sora/src/main/java/magicofconch/sora/util/exception/GlobalExceptionHandler.java
+++ sora/src/main/java/magicofconch/sora/util/exception/GlobalExceptionHandler.java
@@ -1,29 +1,31 @@
 package magicofconch.sora.util.exception;
 
-
 import java.net.URI;
-import lombok.extern.slf4j.Slf4j;
-import magicofconch.sora.util.Response;
-import magicofconch.sora.util.ResponseCode;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import lombok.extern.slf4j.Slf4j;
+import magicofconch.sora.util.Response;
+import magicofconch.sora.util.ResponseCode;
+
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(BusinessException.class)
-    public Response<ResponseCode> handleBusinessException(BusinessException e) {
-        return new Response<>(e.getResponseCode());
-    }
+	@ExceptionHandler(BusinessException.class)
+	public ResponseEntity<?> handleBusinessException(BusinessException e) {
+		ResponseCode responseCode = e.getResponseCode();
+		return ResponseEntity.status(responseCode.getStatus()).body(Response.errorResponse(responseCode));
+	}
 
-    @ExceptionHandler(RedirectException.class)
-    public ResponseEntity handleRedirectException(RedirectException e) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(URI.create(e.getRedirectURI()));
-        return new ResponseEntity(httpHeaders, HttpStatus.TEMPORARY_REDIRECT);
-    }
+	@ExceptionHandler(RedirectException.class)
+	public ResponseEntity handleRedirectException(RedirectException e) {
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setLocation(URI.create(e.getRedirectURI()));
+		return new ResponseEntity(httpHeaders, HttpStatus.TEMPORARY_REDIRECT);
+	}
 }
