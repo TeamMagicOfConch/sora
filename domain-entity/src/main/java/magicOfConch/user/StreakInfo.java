a608f5f28992ac7af2387414678b795619e221fa
greensnapback0229 <smdmim@gmail.com>
2025-09-10 00:21:56 +0900

    feat: streak 정보 엔티티 추가 및 맵핑

---
+++ domain-entity/src/main/java/magicOfConch/user/StreakInfo.java
@@ -0,0 +1,47 @@
+package magicOfConch.user;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.Comment;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "streak_info")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StreakInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "streak_info_id")
+	private Long id;
+
+	@OneToOne(mappedBy = "streakInfo", fetch = FetchType.LAZY)
+	private UserInfo userInfo;
+
+	@Column(length = 25)
+	@Comment("사용자가 정의한 리뷰 작성시간명")
+	private String reviewTime;
+
+	@Comment("사용자가 정의한 리뷰 작성 시간")
+	private LocalDateTime reviewAt;
+
+	@Column(length = 25)
+	@Comment("희망 리뷰 작성 장소")
+	private String writeLocation;
+
+	@Column(length = 25)
+	@Comment("사용자 목표 정체성")
+	private String aspiration;
+}
