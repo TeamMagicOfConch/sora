cdc98cfd58a8cf84984c844e3a7c408ed3311963
greensnapback0229 <smdmim@gmail.com>
2025-10-22 21:42:59 +0900

    fix: 기존 리뷰 기능 롤백

---
+++ sora/src/main/java/magicofconch/sora/review/api/controller/SoraController.java
@@ -0,0 +1,89 @@
+package magicofconch.sora.review.api.controller;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.springframework.http.MediaType;
+import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import magicofconch.sora.review.api.dto.req.SubmitReq;
+import magicofconch.sora.review.api.dto.res.ReviewRes;
+import magicofconch.sora.review.service.SoraService;
+import magicofconch.sora.util.ResponseCode;
+import magicofconch.sora.util.exception.BusinessException;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class SoraController {
+
+	private final SoraService soraService;
+
+	@PostMapping(value = "/auth/user/api/review/submit", produces = MediaType.TEXT_EVENT_STREAM_VALUE, consumes = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public SseEmitter submitReview(@RequestBody String requestBody, HttpServletResponse response) {
+		SubmitReq req;
+
+		try {
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.registerModule(new JavaTimeModule());
+			req = objectMapper.readValue(requestBody, SubmitReq.class);
+
+		} catch (Exception e) {
+			throw new BusinessException(ResponseCode.REVIEW_JSON_ERROR);
+		}
+
+		SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+
+		response.setHeader("X-Accel-Buffering", "no");
+		response.setContentType(MediaType.TEXT_EVENT_STREAM_VALUE);
+
+		SseEmitter emitter = new SseEmitter(60 * 1000L);
+
+		emitter.onCompletion(() -> System.out.println("SSE connection completed"));
+		emitter.onTimeout(() -> {
+			System.out.println("SSE connection timed out");
+			emitter.complete();
+		});
+
+		String feedback = soraService.requestSora(req);
+
+		ExecutorService executorService = Executors.newSingleThreadExecutor();
+		ExecutorService delegatingExecutor = new DelegatingSecurityContextExecutorService(executorService);
+		SecurityContext securityContext = SecurityContextHolder.getContext();
+		delegatingExecutor.submit(() -> {
+			SecurityContextHolder.setContext(securityContext);
+			try {
+				int seq = 0;
+				for (char part : feedback.toCharArray()) {
+					ReviewRes res = ReviewRes.builder()
+						.value(String.valueOf(part))
+						.seq(seq)
+						.build();
+					emitter.send(res);
+					seq++;
+				}
+				emitter.complete();
+			} catch (IOException e) {
+				log.warn("sse execute error");
+				emitter.completeWithError(e);
+			} finally {
+				executorService.shutdown();
+			}
+		});
+
+		return emitter;
+	}
+}
