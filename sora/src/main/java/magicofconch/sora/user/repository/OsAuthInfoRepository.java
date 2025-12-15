a9098273585132cc4754c5133d376de0c865a992
greensnapback0229 <smdmim@gmail.com>
2025-12-15 20:15:06 +0900

    feat: token 등록 로직추가

--- sora/src/main/java/magicofconch/sora/user/repository/OsAuthInfoRepository.java
+++ sora/src/main/java/magicofconch/sora/user/repository/OsAuthInfoRepository.java
@@ -3,11 +3,24 @@ package magicofconch.sora.user.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import magicOfConch.user.OsAuthInfo;
 
 public interface OsAuthInfoRepository extends JpaRepository<OsAuthInfo, Long> {
 	Optional<OsAuthInfo> findOsAuthInfoByOsId(String osId);
+
+	@Query("""
+		    SELECT o
+		    FROM OsAuthInfo o
+		    WHERE o.userInfo.id = :userInfoId
+		      AND o.osId = :osId
+		""")
+	Optional<OsAuthInfo> findByUserIdAndOsId(
+		@Param("userInfoId") Long userInfoId,
+		@Param("osId") String osId
+	);
 
 	Boolean existsByOsId(String identifier);
 }
