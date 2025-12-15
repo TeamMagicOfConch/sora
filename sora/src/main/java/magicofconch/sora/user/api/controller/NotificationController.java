d20575862f7633bc7a0e6a4f3e807964767ea9ce
greensnapback0229 <smdmim@gmail.com>
2025-12-15 20:27:23 +0900

    feat: notification 등록 메서드 추가

--- sora/src/main/java/magicofconch/sora/user/api/controller/NotificationController.java
+++ sora/src/main/java/magicofconch/sora/user/api/controller/NotificationController.java
@@ -37,6 +37,7 @@ public class NotificationController {
 	 * @param req : 알림 설정 값
 	 * @return : 설정 성공 여부
 	 */
+	@PostMapping
 	public ResponseEntity<Response> updateNotification(@RequestBody NotificationInfoReq req) {
 		notificationService.updateNotification(req);
 
